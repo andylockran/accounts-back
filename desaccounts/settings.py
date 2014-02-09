6697bee456c666e097191e0405bd66780db5704d
Andy Loughran <aloughran@scorecomms.com>
2014-02-09 19:04:30 +0000

    Initial model build

--- desaccounts/settings.py
+++ desaccounts/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ledger',
+    'djangular',
 )
 
 MIDDLEWARE_CLASSES = (
--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+class Expense(models.Model):
+    "This model handles each of the potential expense lines in an invoice."
+    EXPENSE_TYPES = (
+        ('BANK', 'BANK'),
+        ('CASH', 'CASH'),
+    )
+    #Field Definitions
+    expenseType = models.CharField(null=False,blank=False,max_length=4,choices=EXPENSE_TYPES)
+    date = models.DateField(null=False, blank=False)
+    amount = models.DecimalField(null=False,blank=False,decimal_places=2,max_digits=10)
+    supplierCode = models.ForeignKey('Supplier', null=True, blank=True,max_length="40")
+    nominalCode = models.CharField(null=True, blank=True,max_length="40")
+    invoiceRef = models.CharField(null=True, blank=True,max_length="40")
+    VATrate = models.CharField(null=True, blank=True,max_length="40")
+    VAT = models.IntegerField(null=True, blank=True,max_length="40")
+    EU = models.CharField(null=True, blank=True,max_length="40")
+    toBeClaimedExpense = models.NullBooleanField(null=True, blank=True)
+
+    def __unicode__(self):
+       return str(self.id)
+
+class Supplier(models.Model):
+    name = models.CharField(max_length="255")
+    code = models.CharField(max_length="8")
+
+    def __unicode__(self):
+       return self.code
+
+class Mileage(models.Model):
+   date = models.DateField(null=False,blank=False)
+   miles = models.IntegerField(null=True,blank=True)
+   fromLocation = models.CharField(max_length=40)
+   toLocation = models.CharField(max_length=40)
+   customer = models.ForeignKey('Customer',blank=True,null=True)
+   reason = models.TextField(max_length="1000")
+   notes = models.TextField(max_length="1000")
+   toBeClaimedExpense = models.NullBooleanField(null=True, blank=True)
+
+   def __unicode__(self):
+      return str(self.id)
+
+class RecordSalesInvoice(models.Model):
+   invoiceReference = models.CharField(max_length="40")
+   date = models.DateField()
+   customer = models.ForeignKey('Customer')
+   amount = models.DecimalField(null=False,blank=False,decimal_places=2,max_digits=10)
+   dueDate = models.DateField(blank=True,null=True)
+   paid = models.BooleanField()
+   #linkTo = @TODO
+   def __unicode__(self):
+      return str(self.id)
+
+class RecordSupplierInvoice(models.Model):
+   orderReference = models.CharField(max_length="255")
+   invoiceReference = models.CharField(max_length="40")
+   date = models.DateField()
+   customer = models.ForeignKey('Customer')
+   amount = models.DecimalField(null=False,blank=False,decimal_places=2,max_digits=10)
+   dueDate = models.DateField(blank=True,null=True)
+   paid = models.BooleanField()
+   #linkTo = @TODO
+   def __unicode__(self):
+      return str(self.id)
+
+class Customer(models.Model):
+   name = models.CharField(max_length="255")
+   code = models.CharField(max_length="8")
+   def __unicode__(self):
+      return self.code
+
+
+admin.site.register(Expense)
+admin.site.register(Supplier)
+admin.site.register(Customer)
+admin.site.register(Mileage)
+admin.site.register(RecordSalesInvoice)
+admin.site.register(RecordSupplierInvoice)
